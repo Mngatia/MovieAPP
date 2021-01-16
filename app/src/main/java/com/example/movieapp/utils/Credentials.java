290af2b177cefa78278b386296eacb90d5a374dc
Mngatia <nyawirahngatia@gmail.com>
2021-01-16 16:16:40 +0300

    Credentials class and adding Retrofit.

--- app/src/main/java/com/example/movieapp/utils/Credentials.java
+++ app/src/main/java/com/example/movieapp/utils/Credentials.java
@@ -1,4 +1,7 @@
 package com.example.movieapp.utils;
 
 public class Credentials {
+
+    public static final String BASE_URL = "http://api.themoviedb.org/";
+    public static final String API_KEY = "e4989174a6558954c59d7b26abaf074c";
 }
