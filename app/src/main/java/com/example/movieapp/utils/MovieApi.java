b257be4dfa6c4e03896fb4de7f960d9b23bb49cd
Mngatia <nyawirahngatia@gmail.com>
2021-01-17 20:02:03 +0300

    Model Class for API.

--- app/src/main/java/com/example/movieapp/utils/MovieApi.java
+++ app/src/main/java/com/example/movieapp/utils/MovieApi.java
@@ -1,4 +1,20 @@
 package com.example.movieapp.utils;
 
+import com.example.movieapp.response.MovieSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface MovieApi {
+    //Search for movies
+    //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+    //http://api.themoviedb.org/3/movie/popular?api_key=e4989174a6558954c59d7b26abaf074c&page=1
+
+    @GET
+    Call<MovieSearchResponse> searchMovie(
+            @Query("api_key") String key,
+            @Query("query") String query,
+            @Query("page") String page
+    );
 }
